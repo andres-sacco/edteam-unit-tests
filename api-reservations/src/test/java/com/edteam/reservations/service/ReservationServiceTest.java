c627869af9f1666e6a4c8265aeb6754718c2966f
asacco <andres.sacco@travelx.io>
2024-01-30 15:26:41 -0300

    final

--- api-reservations/src/test/java/com/edteam/reservations/service/ReservationServiceTest.java
+++ api-reservations/src/test/java/com/edteam/reservations/service/ReservationServiceTest.java
@@ -1,20 +1,14 @@
 package com.edteam.reservations.service;
 
+import com.edteam.reservations.dto.*;
 import com.edteam.reservations.enums.APIError;
 import com.edteam.reservations.exception.EdteamException;
-import com.edteam.reservations.model.*;
 import com.edteam.reservations.repository.ReservationRepository;
 import org.junit.jupiter.api.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tags(@Tag("service"))
@@ -40,8 +34,58 @@ class ReservationServiceTest {
                 () -> assertNotNull(exception),
                 () -> assertEquals(APIError.RESERVATION_NOT_FOUND.getMessage(), exception.getDescription()),
                 () -> assertEquals(APIError.RESERVATION_NOT_FOUND.getHttpStatus(), exception.getStatus())
-
         );
+    }
+
+    @Disabled
+    @Tag("success-case")
+    @DisplayName("should return the information of the reservation")
+    @Test
+    void getReservation_should_return_the_information() {
+
+        // Given
+        ReservationRepository repository = new ReservationRepository();
+        ReservationService service = new ReservationService(repository, null, null);
+
+        // When
+        ReservationDTO result = service.getReservationById(1L);
+
+        // Then
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertEquals(getReservation(1L, "EZE", "MIA"), result));
+    }
+
+    private ReservationDTO getReservation(Long id, String origin, String destination) {
+        PassengerDTO passenger = new PassengerDTO();
+        passenger.setFirstName("Andres");
+        passenger.setLastName("Sacco");
+        passenger.setDocumentType("DNI");
+        passenger.setDocumentNumber("12345678");
+        passenger.setBirthday(LocalDate.of(1985, 1, 1));
+
+        PriceDTO price = new PriceDTO();
+        price.setBasePrice(BigDecimal.ONE);
+        price.setTotalTax(BigDecimal.ZERO);
+        price.setTotalPrice(BigDecimal.ONE);
+
+        SegmentDTO segment = new SegmentDTO();
+        segment.setArrival("2025-01-01");
+        segment.setDeparture("2024-12-31");
+        segment.setOrigin(origin);
+        segment.setDestination(destination);
+        segment.setCarrier("AA");
+
+        ItineraryDTO itinerary = new ItineraryDTO();
+        itinerary.setPrice(price);
+        itinerary.setSegment(List.of(segment));
+
+        ReservationDTO reservation = new ReservationDTO();
+        reservation.setId(id);
+        reservation.setPassengers(List.of(passenger));
+        reservation.setItinerary(itinerary);
+
+        return reservation;
     }
 
 }
