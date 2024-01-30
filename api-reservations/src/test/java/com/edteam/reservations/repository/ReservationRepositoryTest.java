65787eeaf16c9a0a5d91ff7cc4be314f547d77ed
asacco <andres.sacco@travelx.io>
2024-01-30 16:53:54 -0300

    final

--- api-reservations/src/test/java/com/edteam/reservations/repository/ReservationRepositoryTest.java
+++ api-reservations/src/test/java/com/edteam/reservations/repository/ReservationRepositoryTest.java
@@ -2,6 +2,10 @@ package com.edteam.reservations.repository;
 
 import com.edteam.reservations.model.*;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,6 +46,68 @@ class ReservationRepositoryTest {
     }
 
     @Tag("success-case")
+    @DisplayName("should return the information of all the reservations using external files")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/save-repository.csv")
+    void save_should_return_the_information_using_external_file(String origin, String destination) {
+
+        // Given
+        ReservationRepository repository = new ReservationRepository();
+
+        // When
+        Reservation reservation = repository.save(getReservation(null, origin, destination));
+
+        // Then
+        assertAll(
+                () -> assertNotNull(reservation),
+                () -> assertEquals(origin, reservation.getItinerary().getSegment().get(0).getOrigin()),
+                () -> assertEquals(destination, reservation.getItinerary().getSegment().get(0).getDestination())
+        );
+    }
+
+    @Tag("success-case")
+    @DisplayName("should return the information of all the reservations using CSV")
+    @ParameterizedTest
+    @CsvSource({"MIA,AEP", "BUE,SCL", "BUE,MIA"})
+    void save_should_return_the_information_using_csv(String origin, String destination) {
+
+        // Given
+        ReservationRepository repository = new ReservationRepository();
+
+        // When
+        Reservation reservation = repository.save(getReservation(null, origin, destination));
+
+        // Then
+        assertAll(
+                () -> assertNotNull(reservation),
+                () -> assertEquals(origin, reservation.getItinerary().getSegment().get(0).getOrigin()),
+                () -> assertEquals(destination, reservation.getItinerary().getSegment().get(0).getDestination())
+        );
+    }
+
+    @Tag("success-case")
+    @DisplayName("should return the information of all the reservations using parameters")
+    @ParameterizedTest
+    @ValueSource(strings = {"AEP", "MIA"})
+    void save_should_return_the_information_using_parameters(String origin) {
+
+        // Given
+        ReservationRepository repository = new ReservationRepository();
+
+        // When
+        Reservation reservation = repository.save(getReservation(null, origin, "MIA"));
+
+        // Then
+        assertAll(
+                () -> assertNotNull(reservation),
+                () -> assertEquals(origin, reservation.getItinerary().getSegment().get(0).getOrigin()),
+                () -> assertEquals("MIA", reservation.getItinerary().getSegment().get(0).getDestination())
+        );
+    }
+
+
+    @Disabled
+    @Tag("success-case")
     @DisplayName("should return the information of all the reservations")
     @Test
     void getReservations_should_return_the_information() {
@@ -77,7 +143,9 @@ class ReservationRepositoryTest {
         Optional<Reservation> result = repository.getReservationById(1L);
 
         // Then
-        assertAll(() -> assertNotNull(result), () -> assertTrue(result.isPresent()),
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertTrue(result.isPresent()),
                 () -> assertEquals(getReservation(1L, "EZE", "MIA"), result.get()));
     }
 
